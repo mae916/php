0f3507d694e32d0ef1e9559e727185f2db807664
mae916 <ruchia916@naver.com>
2021-11-18 01:52:27 +0900

    php 학습

---
+++ 강의소스/20211117/ex3_process.php
@@ -0,0 +1,19 @@
+<?php
+echo "<pre>";
+print_r($_SERVER);
+echo "</pre>";
+
+echo "<h1>POST</h1>";
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+echo "<h1>GET</h1>";
+echo "<pre>";
+print_r($_GET);
+echo "</pre>";
+
+echo "<h1>REQUEST</h1>";
+echo "<pre>";
+print_r($_REQUEST);
+echo "</pre>";
